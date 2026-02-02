c4d457aa2a129743d2395fa8dd121936987028f3
Nobberu <itsnobberu@gmail.com>
2026-02-02 21:35:50 +0700

    Minor fix

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,12 +1,12 @@
-import { MetadataRoute } from "next";
-
+import { MetadataRoute } from 'next'
+ 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://nobberu.vercel.app",
+      url: 'https://nobberu.vercel.app',
       lastModified: new Date(),
-      changeFrequency: "monthly",
+      changeFrequency: 'daily',
       priority: 1,
     },
-  ];
+  ]
 }
