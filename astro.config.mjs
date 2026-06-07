42e22dd781ed312051675b86459aeab3fb96aba6
Nobberu <itsnobberu@gmail.com>
2026-06-07 11:32:24 +0700

    Feat: SEO improvements attempt

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-
-import tailwindcss from '@tailwindcss/vite';
-
-import svelte from '@astrojs/svelte';
+import { defineConfig } from "astro/config";
+import tailwindcss from "@tailwindcss/vite";
+import svelte from "@astrojs/svelte";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://nobberu.me",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
-
-  integrations: [svelte()]
+  integrations: [svelte(), sitemap()],
 });
